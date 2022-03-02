606d941f21558badd969e2dbae37cfc3abf5b180
KevinTMO <kevin.ramos3@hotmail.com>
2022-03-02 14:26:47 -0400

    Dummby data in flask to pass info for testing in home signup, home.html for everything related to that route plus adjusted with jinja and layout html for everything redundant on every other route

--- app/flaskApp.py
+++ app/flaskApp.py
@@ -2,10 +2,34 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+forms = [
+    {
+        'name': 'Sergio',
+        'email': 'sergiovera@whatever.com',
+        'password': 'funkified',
+        'confirm': 'funkified'
+    },
+
+    {
+        'name': 'Yared',
+        'email': 'yaredtorres@whatever.com',
+        'password': 'partychu',
+        'confirm': 'partychu'
+    }
+]
+
+'''
+numbers = [
+    {
+        'nmbr': [1, 2]
+    }
+]
+'''
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('main.html')
+    return render_template('home.html', forms=forms, title='Artist')
 
 
 if __name__ == "__main__":
