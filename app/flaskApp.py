210351f52b47f62dfb4ec93f4f92a94be48db84a
KevinTMO <kevin.ramos3@hotmail.com>
2022-03-03 12:40:42 -0400

    New home.html, removing redudant code in layout, adding login form, fixing wrong attr in forms.py and removing everything not useful in flask

--- app/flaskApp.py
+++ app/flaskApp.py
@@ -1,72 +1,50 @@
 # from datetime import datetime
-from flask import Flask, render_template, url_for, flash, redirect
 # from flask_sqlalchemy import SQLALCHEMY_DATABASE_URL
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = '73be4410e3006053dfe913a29efe79be'
 # APP.CONFIG['SQLALCHEMY_DATABASE_URL'] = ''
 # db = SQLAlchemy(app)
 
 
-forms = [
-    {
-        'name': 'Sergio',
-        'email': 'sergiovera@whatever.com',
-        'password': 'funkified',
-        'confirm': 'funkified'
-    },
+'''Below route methods '''
 
-    {
-        'name': 'Yared',
-        'email': 'yaredtorres@whatever.com',
-        'password': 'partychu',
-        'confirm': 'partychu'
-    }
-]
-
-'''
-numbers = [
-    {
-        'nmbr': [1, 2]
-    }
-]
-'''
-
-
-def __repr__(self):
-    return f"Post('{self.title}', '{self.date_posted}')"
-
-
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html', forms=forms, title='Artist')
+    """
+    def landing page route using only domain or route in browser
+    """
+    form = RegistrationForm()
 
-"""
-For Ilustration Purposes-->
-----------------------------
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-    def __repr__(self):
-        return "User'{}', '{}', '{}')".format(self.username, self.email), self.image_file)
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'sergiovera@whatever.com' and form.password.data == 'password':
+        flash(f'Account created for { form.name.data }!', 'success')
+        return redirect(url_for('login'))
+    return render_template('home.html', form=form)
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """
+    def login for redirection test after registration of usr
+    """
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        if form.email.data == "admin@artist.com" and form.password.data == 'password':
             flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
-"""
+
+
+''' Below flask ip/port config '''
+
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5000, debug=True)
--- app/forms.py
+++ app/forms.py
@@ -8,7 +8,7 @@ class RegistrationForm(FlaskForm):
     """
     name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
@@ -17,6 +17,6 @@ class LoginForm(FlaskForm):
     Creating the login attrs from the usr/artist
     """
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Log In')
