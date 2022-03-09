f3b2de9a8ebb5664431d02ba2952bef2afe6e216
KevinTMO <kevin.ramos3@hotmail.com>
2022-03-09 14:31:01 -0400

    Adding a new database for genre/instrument based on binds from sqlalchemy

--- v1/models.py
+++ v1/models.py
@@ -1,5 +1,6 @@
-from v1 import db, login_manager
+from v1 import db, login_manager, app
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 
 @login_manager.user_loader
@@ -26,6 +27,27 @@ class Artist(db.Model, UserMixin):
     instrument = db.Column(db.String(50))
     biography = db.Column(db.String(150))
 
+
+    def get_reset_token(self, expires_sec=1800):
+        """
+        Generating a token that expires every 3 mins by def
+        Can pass a value to overwrite that value for another
+        """
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        """
+        Method to verify the token or if its expired
+        """
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return Artist.query.get(user_id)
+
     def __repr__(self):
         """
         return repr string of Artist obj
@@ -37,6 +59,7 @@ class Gen(db.Model):
     """
     sql table just for genre column select field testing
     """
+    __bind_key__ = 'dropquery'
     id = db.Column(db.Integer, primary_key=True)
     genre = db.Column(db.String(50))
 
@@ -51,6 +74,7 @@ class Inst(db.Model):
     """
     sql table just for instrument column select field testing
     """
+    __bind_key__ = 'dropquery'
     id = db.Column(db.Integer, primary_key=True)
     instrument = db.Column(db.String(50))
 
