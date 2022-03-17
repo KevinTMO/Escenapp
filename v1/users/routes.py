4f32eeabf5ca7683c7436d91574c55b67438c43d
KevinTMO <kevin.ramos3@hotmail.com>
2022-03-17 13:44:05 -0400

    A new template named update_account with form to update email, bio, and picture plus account will now reflect profile picture changes

--- v1/users/routes.py
+++ v1/users/routes.py
@@ -60,9 +60,9 @@ def account():
     """
     User account route
     """
+    events = PostEvent.query.all() # get all events from db
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file,
-                           form=form, formE=formE, events=events)
+    return render_template('account.html', title='Account', image_file=image_file)
 
 
 
@@ -78,7 +78,6 @@ def updateAccount():
     """
     form = UpdateAccountForm()
     formE = UpdateEmailForm()
-    events = PostEvent.query.all() # get all events from db
     if form.validate_on_submit():
         if form.picture.data:
             picture_file = save_picture(form.picture.data)
@@ -90,6 +89,8 @@ def updateAccount():
         db.session.commit()
         flash('Your account has been updated', 'success')
         return redirect(url_for('users.account'))
+    return render_template('update_account.html', title='Update Account', form=form,
+                           formE=formE)
 
 
 
